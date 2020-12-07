a9a77cc0f9f2fa1d0845ebd568fc63b5c52423d5
Camilla Russell <milla_russell@hotmail.com>
2020-12-07 13:14:57 -0300

    adicionando css ao detalhe do autor

--- js/detalhe-autor.js
+++ js/detalhe-autor.js
@@ -5,7 +5,7 @@ const cardResult = document.querySelector("#card-result");
 function meuCallback(authorData) {
   console.log(authorData);
   cardResult.innerHTML = `
-<div>
+<div id="author-card">
 <h2>${authorData.authordisplay}</h2>
 <p><strong>Sobre o autor (apenas em inglês):</strong>
 <br/>
--- js/detalhe-livro.js
+++ js/detalhe-livro.js
@@ -4,11 +4,11 @@ const cardResult = document.querySelector("#card-result");
 
 function meuCallback(bookData) {
   console.log(bookData);
-    cardResult.innerHTML = `
+  cardResult.innerHTML = `
   <div class="left-card">
   <h2><span id="title-card">Título:</span> ${bookData.titleweb}</h2>
   <p><strong>Autor:</strong> ${bookData.authorweb}</p>
-  <p><strong>Data de publicação</strong> ${bookData.onsaledate}</p>
+  <p><strong>Data de publicação:</strong> ${bookData.onsaledate}</p>
   <p>${bookData.pages} páginas</p>
   </div>
   <div class="right-card">
