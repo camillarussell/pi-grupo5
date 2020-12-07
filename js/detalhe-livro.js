3bb9b729f1019c4cef78b848d2fe3ef18d8db7dd
Camilla Russell <milla_russell@hotmail.com>
2020-12-07 11:47:41 -0300

    corrigindo detalhe dos livros

--- js/detalhe-livro.js
+++ js/detalhe-livro.js
@@ -4,17 +4,17 @@ const cardResult = document.querySelector("#card-result");
 
 function meuCallback(bookData) {
   console.log(bookData);
-  cardResult.innerHTML = `
-<div class="left-card">
-<h2><span id="title-card">Título:</span> ${bookData.titleweb}</h2>
-<p><strong>Autor:</strong> ${bookData.authorweb}</p>
-<p><strong>Data de publicação</strong> ${bookData.onsaledate}</p>
-<p>${bookData.pages} páginas</p>
-</div>
-<div class="right-card">
-<img src="${bookData["@uri"]}">
-</div>
-`;
+    cardResult.innerHTML = `
+  <div class="left-card">
+  <h2><span id="title-card">Título:</span> ${bookData.titleweb}</h2>
+  <p><strong>Autor:</strong> ${bookData.authorweb}</p>
+  <p><strong>Data de publicação</strong> ${bookData.onsaledate}</p>
+  <p>${bookData.pages} páginas</p>
+  </div>
+  <div class="right-card">
+  <img src="${bookData["@uri"]}">
+  </div>
+  `;
 }
 
 getBookDetails(bookId, meuCallback);
